cc5a2b2367d7732bef557d38bfae6e49256a9f35
Vicki <vgercans@gmail.com>
2015-11-16 16:59:20 -0600

    Checkpoint-19-complete

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by victoria Gercans on 11/16/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by victoria Gercans on 11/16/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
