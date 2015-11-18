888c72ae7b1d194b27976db2d2cc77b575524c71
Vicki <vgercans@gmail.com>
2015-11-17 20:06:24 -0600

    Checkpoint-20-complete

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by victoria Gercans on 11/17/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
